215ce834ca62dda630347158abf73335915623ac
Eric Brown <quadtree@gmail.com>
2016-04-02 18:17:42 +0000

    CSV pre-processing added

--- build.py
+++ build.py
@@ -9,63 +9,108 @@ import http.client
 import hashlib
 import urllib.parse
 import gzip
-	
+import csv
+import json
+
+def convertCsvToTmpJs(csvFile):
+	tmpFile = "/tmp/" + hashlib.sha1(csvFile.encode("utf-8")).hexdigest() + ".csv.cache.js"
+
+	m = re.match('.+?/([^/"]+)\.csv', csvFile, re.IGNORECASE)
+	keyName = m.group(1)
+
+	print("Converting " + csvFile + " to tmp JS " + tmpFile + ", keyname is " + keyName)
+
+	fi = csv.reader(open(csvFile))
+
+	columnNames = None
+	outData = []
+
+	for row in fi:
+		if (not columnNames):
+			columnNames = row
+		else:
+			parsedRow = {}
+
+			for i in range(len(columnNames)):
+				parsedRow[columnNames[i]] = row[i]
+
+			outData.append(parsedRow)
+
+
+	fo = open(tmpFile, "w")
+	fo.write('"use strict";\n')
+	fo.write('if (typeof(data) == "undefined") var data = {};\n');
+	fo.write('data["' + keyName + '"] = ' + json.dumps(outData, "\t") + ';\n')
+	fo.close()
+
+	return tmpFile
+
+def preprocessCsv():
+	newScriptFiles = []
+
+	for (dirPath, dirNames, fileNames) in os.walk(root):
+		for fn in fileNames:
+			if (fn.lower().endswith(".csv")):
+				newScriptFiles.append(convertCsvToTmpJs(dirPath + "/" + fn))
+
+	return newScriptFiles
+
 def hashFile(file):
 	f = open(file, "rb")
-	
+
 	h = hashlib.sha1()
-	
+
 	for line in f:
 		h.update(line + "\n".encode("utf-8"))
-		
+
 	print (file + " ==SHA1=> " + h.hexdigest())
-	
+
 	return h.hexdigest()
 
 def performMinification(command, fileListRaw, ext, indiv=False):
 
 	fileList = []
-	
+
 	overallHash = hashlib.sha1()
 
 	for file in fileListRaw:
 		if (file[0:5] == "https"):
 			if (ext != "css"):
 				pr = urllib.parse.urlparse(file)
-			
+
 				cacheFile = "/tmp/" + hashlib.sha1(file.encode("utf-8")).hexdigest() + ".cache." + ext
-				
+
 				if (not os.path.isfile(cacheFile)):
 					print ("Downloading " + file)
-					
+
 					con = http.client.HTTPSConnection(pr.netloc)
 					con.request("GET", pr.path)
-					
+
 					respText = con.getresponse().read()
-					
+
 					f = open(cacheFile, "wb")
 					f.write(respText)
 					f.close()
-					
+
 					print("Downloaded and saved " + str(len(respText)) + " bytes to " + cacheFile)
-					
+
 				file = cacheFile
 			else:
 				file = None
-		else:
+		elif (file[0] != "/"):
 			file = root + "/" + file
-		
+
 		if (file):
 			fileList.append(file)
-			
+
 			overallHash.update(hashFile(file).encode("utf-8"))
-	
+
 	output = ""
-	
+
 	outFileStub = "dist/combined-" + overallHash.hexdigest() + ".min." + ext
 	outFileEnd = outFileStub + ".gz"
 	outFile = root + "/" + outFileEnd
-	
+
 	if (indiv):
 		raise Exception("EX")
 	else:
@@ -73,7 +118,7 @@ def performMinification(command, fileListRaw, ext, indiv=False):
 			args = [command] + fileList
 			print ("args: " + str(args))
 			output = subprocess.check_output(args).decode("utf-8")
-			
+
 			f = gzip.open(outFile, "wb")
 			f.write(output.encode("utf-8"))
 			f.close()
@@ -81,14 +126,14 @@ def performMinification(command, fileListRaw, ext, indiv=False):
 			args = [command, '-o', root + "/" + outFileStub] + fileList
 			print ("args: " + str(args))
 			subprocess.check_call(args)
-			
+
 			args = ['gzip', root + "/" + outFileStub]
 			print ("args: " + str(args))
 			subprocess.check_call(args)
-	
-	
+
+
 	print("Successfully built " + outFile + " contains " + str(len(output)) + " characters")
-	
+
 	return outFileEnd
 
 root = os.path.dirname(os.path.realpath(sys.argv[1]))
@@ -115,25 +160,27 @@ class CustomHTMLParser(html.parser.HTMLParser):
 					href = v
 				if (k == "rel" and v == "stylesheet"):
 					isStylesheet = True
-			
+
 			if (isStylesheet):
 				cssFiles.append(href)
 	def handle_endtag(self, tag):
 		pass
 	def handle_data(self, data):
 		pass
-		
+
 parser = CustomHTMLParser()
 
 f = open(sys.argv[1], "r")
 
 for line in f:
 	parser.feed(line)
-	
+
 f.close()
 
 print(str(scriptFiles))
 print(str(cssFiles))
+
+scriptFiles = preprocessCsv() + scriptFiles
 
 jsOutFile = performMinification('/usr/local/bin/closure-compiler', scriptFiles, 'js')
 cssOutFile = performMinification('/usr/local/bin/cleancss', cssFiles, 'css')
@@ -151,33 +198,33 @@ class RebuildingHTMLParser(html.parser.HTMLParser):
 	def handle_starttag(self, tag, attrs):
 		if (tag == "script"):
 			return
-		
+
 		if (tag == "link"):
 			isStylesheet = False
 			isExternal = False
-		
+
 			for (k,v) in attrs:
 				if (k == "rel" and v == "stylesheet"):
 					isStylesheet = True
 				if (k == "href" and v[:5] == "https"):
 					isExternal = True
-					
+
 			if (isStylesheet and not isExternal):
 				return
-		
+
 		outHtml.write("<" + tag)
-		
+
 		for (k,v) in attrs:
 			if (v):
 				outHtml.write(' ' + k + '="' + v + '"')
 			else:
 				outHtml.write(' ' + k)
-		
+
 		if (tag == "link" or tag == "br"):
 			outHtml.write("/>")
 		else:
 			outHtml.write(">")
-			
+
 		if (tag == "head"):
 			outHtml.write('<script src="' + jsOutFile + '" async></script>\n')
 			outHtml.write('<link rel="stylesheet" href="' + cssOutFile + '"/>\n')
@@ -188,12 +235,12 @@ class RebuildingHTMLParser(html.parser.HTMLParser):
 		outHtml.write(data)
 
 parser = RebuildingHTMLParser()
-		
+
 f = open(sys.argv[1], "r")
 
 for line in f:
 	parser.feed(line)
-	
+
 f.close()
 
 
